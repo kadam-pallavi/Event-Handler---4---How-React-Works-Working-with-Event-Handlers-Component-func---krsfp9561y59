5aec0a8be03c4166cb0df3e4fbdacf6d0da01ae5
kadam-pallavi <117823209+kadam-pallavi@users.noreply.github.com>
2023-02-21 23:54:34 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+
+ReactDOM.render(<App />, document.getElementById("root"));
